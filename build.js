9670e6091c27a804e49802f1833fda278ba549e0
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-11-11 22:53:11 +0000

    Let user know they are using debug mode

--- build.js
+++ build.js
@@ -7,7 +7,10 @@ bundle.add('./main')
 
 var options = {}
 var debug = (-1 !== process.argv.indexOf('-d'))
-if (true === debug) options.debug = true
+if (true === debug) {
+  options.debug = true
+  console.log('Building in debug mode...')
+}
 
 bundle.bundle(options, function (err, js) {
     if (err) console.log(err)
